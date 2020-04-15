b5999aa01972c102700b4d2b4414a8d17a0e996a
AllenChinese <13402202685@163.com>
2020-04-15 17:33:33 +0800

    feat: Organize the whole station navigation

--- src/layout/components/Aside.vue
+++ src/layout/components/Aside.vue
@@ -1,51 +1,25 @@
 <template>
   <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
+    <div class="logo-container"></div>
+    <el-menu
+      :default-openeds="['1']"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
       <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航一</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
+        <template slot="title">
+          <i class="el-icon-message"></i>资源管理
+        </template>
+        <router-link class="nav-link" to="/dashboard">
+          <el-menu-item index="1-1">文章管理</el-menu-item>
+        </router-link>
       </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
+      <router-link class="nav-link" to="/about">
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i><span>关于我们</span>
+        </el-menu-item>
+      </router-link>
     </el-menu>
   </el-aside>
 </template>
@@ -64,5 +38,15 @@ export default class Aside extends Vue {
 <style lang="less" scoped>
 .el-aside {
   color: #333;
+  .logo-container {
+    width: 100%;
+    height: 60px;
+    background-color: peru;
+  }
+  .nav-link {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
